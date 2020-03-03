264e296b583fc8c2354df3d2de8beed5b2adad7a
Iury Fukuda <iuryfukuda@gmail.com>
2020-03-03 12:09:24 -0300

    add auto complete

--- init.d/go.el
+++ init.d/go.el
@@ -1,10 +1,15 @@
+(use-package go-complete
+  :ensure t
+  )
+
 (use-package go-mode
   :ensure t
-  :mode "\\.go\\'"
+  :requires go-complete
   :config
   (defun go-mode-setup ()
     (add-hook 'before-save-hook 'gofmt-before-save)
     (setq gofmt-command "goimports"))
+  (add-hook 'completion-at-point-functions 'go-complete-at-point)
   (add-hook 'go-mode-hook 'go-mode-setup)
   :bind (("M-." . godef-jump)
 	 ("C-c d" . godoc-at-point)))
