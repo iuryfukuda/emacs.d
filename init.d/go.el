54deb05e0f92c9dbc759ab268c15c7d255da2f16
Iury Fukuda <zbioe@protonmail.com>
2020-05-14 16:16:08 -0300

    global to local bind of go

--- init.d/go.el
+++ init.d/go.el
@@ -7,11 +7,12 @@
 (use-package go-mode
   :ensure t
   :requires (go-complete go-rename)
+  :bind (:map go-mode-map
+         ("M-." . godef-jump)
+	 ("C-c d" . godoc-at-point))
   :config
   (defun go-mode-setup ()
     (add-hook 'before-save-hook 'gofmt-before-save)
     (setq gofmt-command "goimports"))
   (add-hook 'completion-at-point-functions 'go-complete-at-point)
-  (add-hook 'go-mode-hook 'go-mode-setup)
-  :bind (("M-." . godef-jump)
-	 ("C-c d" . godoc-at-point)))
+  (add-hook 'go-mode-hook 'go-mode-setup))
