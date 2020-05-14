83c739707070ed511fa3a13aeaa312b67e7a97bd
Iury Fukuda <zbioe@protonmail.com>
2020-05-14 16:17:43 -0300

    reorganize hook function

--- init.d/go.el
+++ init.d/go.el
@@ -4,6 +4,12 @@
 (use-package go-rename
   :ensure t)
 
+;; Required install goimports
+;; golang.org/x/tools/cmd/goimports
+(defun go-mode-setup ()
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq gofmt-command "goimports"))
+
 (use-package go-mode
   :ensure t
   :requires (go-complete go-rename)
@@ -11,8 +17,7 @@
          ("M-." . godef-jump)
 	 ("C-c d" . godoc-at-point))
   :config
-  (defun go-mode-setup ()
-    (add-hook 'before-save-hook 'gofmt-before-save)
-    (setq gofmt-command "goimports"))
   (add-hook 'completion-at-point-functions 'go-complete-at-point)
   (add-hook 'go-mode-hook 'go-mode-setup))
+
+
