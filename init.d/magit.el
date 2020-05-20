1b23e24dfb4f5a2d0684e319e645089e07c87f01
Iury Fukuda <zbioe@protonmail.com>
2020-05-20 16:56:53 -0300

    magit kill buffers on quit

--- init.d/magit.el
+++ init.d/magit.el
@@ -4,3 +4,12 @@
   ("C-M-g" . magit-dispatch)
   :config
   (add-hook 'after-save-hook 'magit-after-save-refresh-status t))
+
+(defun mu-magit-kill-buffers ()
+  "Restore window configuration and kill all Magit buffers."
+  (interactive)
+  (let ((buffers (magit-mode-get-buffers)))
+    (magit-restore-window-configuration)
+    (mapc #'kill-buffer buffers)))
+
+(bind-key "q" #'mu-magit-kill-buffers magit-status-mode-map)
