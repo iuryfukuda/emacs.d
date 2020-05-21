847b25f7d9f5fb00a263eb5f67f29902fcfd1863
Iury Fukuda <zbioe@protonmail.com>
2020-05-21 16:07:19 -0300

    fix close next window

--- init.d/keys.el
+++ init.d/keys.el
@@ -35,7 +35,6 @@
   "If there are multiple windows, then close the other pane and kill the buffer in it also."
   (interactive)
   (other-window 1)
-  (kill-this-buffer)
   (if (not (one-window-p))
       (delete-window)))
 
