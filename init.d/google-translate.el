fd376815bfbe0802febef648eb9a3ab2698c9037
Iury Fukuda <zbioe@protonmail.com>
2020-05-23 03:40:01 -0300

    remap translate smooth translate key

--- init.d/google-translate.el
+++ init.d/google-translate.el
@@ -1,8 +1,7 @@
 (use-package google-translate
   :ensure t
   :bind
-  ("C-c t" . google-translate-at-point)
-  ("C-c T" . google-translate-smooth-translate)
+  ("C-c t" . google-translate-smooth-translate)
   :init
   (setq google-translate-default-source-language "en")
   (setq google-translate-default-target-language "pt")
