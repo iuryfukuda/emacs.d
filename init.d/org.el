b1934c2daeeeb8ed58ea28066284b45da51af0f8
Iury Fukuda <zbioe@protonmail.com>
2020-05-27 23:42:55 -0300

    increase default latex margin on org-mode

--- init.d/org.el
+++ init.d/org.el
@@ -1,6 +1,7 @@
 (use-package org
   :ensure t
   :config
+  (setq org-latex-packages-alist '(("margin=2cm" "geometry" nil)))
   (require 'ox-md)
   (setq org-export-backends '(org ascii html icalendar latex md odt)))
 
