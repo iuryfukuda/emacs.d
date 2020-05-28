8a36c38c817b0ed483f402051b8b4b8d396c0208
Iury Fukuda <zbioe@protonmail.com>
2020-05-27 23:43:16 -0300

    remove some facilities with C-c to prevent mode assign in keys

--- init.d/keys.el
+++ init.d/keys.el
@@ -4,12 +4,6 @@
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "M-h") 'backward-kill-word)
 
-;; movement in panels
-(global-set-key (kbd "C-c b")  'windmove-left)
-(global-set-key (kbd "C-c f") 'windmove-right)
-(global-set-key (kbd "C-c p")    'windmove-up)
-(global-set-key (kbd "C-c n")  'windmove-down)
-
 ;; set copy to X clipboard (when -nw is used)
 (global-set-key (kbd "C-c w") 'copy-to-clipboard)
 (global-set-key (kbd "C-c y") 'paste-from-clipboard)
