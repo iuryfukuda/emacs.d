0fcdb5b7b9a47ca11f0e798c2a3440ff9a89afef
Iury Fukuda <zbioe@protonmail.com>
2020-05-28 00:05:23 -0300

    add github flavored markdown support to orgmode

--- init.d/org.el
+++ init.d/org.el
@@ -1,5 +1,11 @@
+;; github flavored markdown
+(use-package ox-gfm
+  :ensure t)
+
+;; intialize orgmode
 (use-package org
   :ensure t
+  :requires ox-gfm
   :config
   (setq org-latex-packages-alist '(("margin=2cm" "geometry" nil)))
   (require 'ox-md)
