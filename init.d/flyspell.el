0edbb4c06151a3023472922b884d053899e2031d
Iury Fukuda <zbioe@protonmail.com>
2020-06-02 04:19:39 -0300

    add spell check

---
+++ init.d/flyspell.el
@@ -0,0 +1,19 @@
+(use-package flyspell
+  :ensure t
+  :bind
+  ("<f9>" . flyspell-mode)
+  (:map flyspell-mode-map
+        ("C-ç c" . ispell-word)
+        ("C-ç r" . flyspell-region)
+        ("C-ç e" . flyspell-to-english)
+        ("C-ç p" . flyspell-to-portuguese))
+  :config
+  (setq ispell-program-name "hunspell"))
+
+(defun flyspell-to-portuguese()
+  (interactive)
+  (ispell-change-dictionary "pt_BR"))
+
+(defun flyspell-to-english()
+  (interactive)
+  (ispell-change-dictionary "en_US"))
