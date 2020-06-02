0fa7bc97c2e56af00ae06878a08b5575c98b6e1a
Iury Fukuda <zbioe@protonmail.com>
2020-06-01 21:39:58 -0300

    emphasis markers can span upto 10 lines

--- init.d/org.el
+++ init.d/org.el
@@ -6,6 +6,9 @@
 (use-package org
   :ensure t
   :requires ox-gfm
+  :init
+  (setf (nth 4 org-emphasis-regexp-components) 10)
+  (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
   :config
   (setq org-latex-packages-alist '(("margin=2cm" "geometry" nil)))
   (require 'ox-md)
