49e539e15ebefc8624761dbc6ccca0eac822548f
Iury Fukuda <zbioe@protonmail.com>
2020-06-03 20:57:06 -0300

    flyspell to inside org

--- init.d/org.el
+++ init.d/org.el
@@ -5,7 +5,7 @@
 ;; intialize orgmode
 (use-package org
   :ensure t
-  :requires ox-gfm
+  :requires (ox-gfm flyspell)
   :init
   (setf (nth 4 org-emphasis-regexp-components) 10)
   (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
@@ -13,4 +13,26 @@
   (setq org-latex-packages-alist '(("margin=2cm" "geometry" nil)))
   (require 'ox-md)
   (setq org-export-backends '(org ascii html icalendar latex md odt)))
+
+(use-package flyspell
+  :ensure t
+  :bind
+  ("<f9>" . flyspell-mode)
+  (:map flyspell-mode-map
+        ("C-ç c" . ispell-word)
+        ("C-ç r" . flyspell-region)
+        ("C-ç b" . flyspell-buffer)
+        ("C-ç e" . flyspell-to-english)
+        ("C-ç p" . flyspell-to-portuguese))
+  :config
+  (setq ispell-program-name "hunspell"))
+
+(defun flyspell-to-portuguese()
+  (interactive)
+  (ispell-change-dictionary "pt_BR"))
+
+(defun flyspell-to-english()
+  (interactive)
+  (ispell-change-dictionary "en_US"))
+
 
