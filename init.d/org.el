37f7186076b9fe761a58ae8fb5236c14d8c8da55
Iury Fukuda <zbioe@protonmail.com>
2020-06-10 17:44:32 -0300

    rebind some org keybindings

--- init.d/org.el
+++ init.d/org.el
@@ -14,10 +14,11 @@
   (require 'ox-md)
   (setq org-export-backends '(org ascii html icalendar latex md odt)))
 
+;; Correct text
 (use-package flyspell
   :ensure t
   :bind
-  ("<f9>" . flyspell-mode)
+  ("C-ç f" . flyspell-mode)
   (:map flyspell-mode-map
         ("C-ç c" . ispell-word)
         ("C-ç r" . flyspell-region)
@@ -25,14 +26,15 @@
         ("C-ç e" . flyspell-to-english)
         ("C-ç p" . flyspell-to-portuguese))
   :config
-  (setq ispell-program-name "hunspell"))
+  (setq ispell-program-name "hunspell")
+  (defun flyspell-to-portuguese()
+    (interactive)
+    (ispell-change-dictionary "pt_BR"))
+  (defun flyspell-to-english()
+    (interactive)
+    (ispell-change-dictionary "en_US")))
 
-(defun flyspell-to-portuguese()
-  (interactive)
-  (ispell-change-dictionary "pt_BR"))
 
-(defun flyspell-to-english()
-  (interactive)
-  (ispell-change-dictionary "en_US"))
+
 
 
