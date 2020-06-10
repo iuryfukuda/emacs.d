9be15559284d16c3ed963675beaa7f3bae3de193
Iury Fukuda <zbioe@protonmail.com>
2020-06-10 17:43:17 -0300

    add multiple cursor suppor

---
+++ init.d/multiple-cursors.el
@@ -0,0 +1,7 @@
+(use-package multiple-cursors
+  :ensure t
+  :bind
+  ("C-ç c" . mc/edit-lines)
+  ("C->" . mc/mark-next-like-this)
+  ("C-<" . mc/mark-previous-like-this)
+  ("C-c C-<" . mc/mark-all-like-this))
