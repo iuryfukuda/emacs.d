9e189f3b62e8ae63e9633429892516cedf2067e9
Iury Fukuda <zbioe@protonmail.com>
2020-06-15 00:21:08 -0300

    chagne markdown command

--- init.d/markdown.el
+++ init.d/markdown.el
@@ -4,4 +4,4 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command '("pandoc" "--no-highlight")))
