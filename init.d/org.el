cbca3e3090bcd5dd1ae9c288ce2db9828535d720
Iury Fukuda <zbioe@protonmail.com>
2020-08-03 20:45:46 -0300

    update configs from orgmode

--- init.d/org.el
+++ init.d/org.el
@@ -1,19 +1,29 @@
-;; github flavored markdown
-(use-package ox-gfm
-  :ensure t)
-
 ;; Intialize orgmode
 (use-package org
   :ensure t
-  :requires (ox-gfm flyspell)
-  :init
-  (setf (nth 4 org-emphasis-regexp-components) 10)
-  (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
+  :requires (ox-gfm ox-md)
   :config
-  (setq org-latex-packages-alist '(("margin=2cm" "geometry" nil)))
-  (require 'ox-md)
+  (setq
+   org-startup-indented t
+   org-catch-invisible-edits 'show
+   org-cycle-separator-lines -1
+   calendar-week-start-day 1
+   org-ellipsis "⤵"
+   org-use-speed-commands t
+   org-refile-targets (quote ((nil :maxlevel . 9)
+                              (org-agenda-files :maxlevel . 9)))
+   org-src-tab-acts-natively t
+   org-src-preserve-indentation t
+   org-src-fontify-natively t
+   org-directory (expand-file-name "Org/" user-emacs-directory)
+   org-agenda-files (expand-file-name "agenda" org-directory)
+   org-support-shift-select t
+   org-latex-packages-alist '(("margin=2cm" "geometry" nil)))
   (setq org-export-backends'(org ascii html icalendar latex md odt))
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((shell . t))))
 
+;; github flavored markdown
+(use-package ox-gfm
+  :ensure t)
