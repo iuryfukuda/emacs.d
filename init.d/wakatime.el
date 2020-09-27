c83e6641f2484040476f0a1a047cab5abaeac7e2
Iury Fukuda <zbioe@protonmail.com>
2020-09-27 13:51:36 -0300

    add wakatime mode

---
+++ init.d/wakatime.el
@@ -0,0 +1,14 @@
+;; (use-package wakatime-mode
+;;   :ensure t
+;;   :config
+;;   (global-wakatime-mode))
+
+;; 224b0929-4c31-4449-b57b-34b60efe3bd6
+
+(use-package wakatime-mode
+  :if (executable-find "wakatime")
+  :defer 4
+  :config
+  (progn
+    (setq wakatime-cli-path (executable-find "wakatime"))
+    (global-wakatime-mode 1)))
